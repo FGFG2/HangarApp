f6489510507772634fd96da83a16b5c4910f55f1
mweidenbach <marteyn@gmx.de>
2015-10-27 12:30:40 +0100

    Bluetooth lib neu

---
+++ lib-smartlink/src/main/java/com.tobyrich.dev.hangarapp.lib/connection/events/PlaneResult.java
@@ -0,0 +1,31 @@
+package com.tobyrich.dev.hangarapp.lib.connection.events;
+
+import android.util.Log;
+
+/**
+ * Created by geno on 10/27/15.
+ */
+public class PlaneResult {
+    private static final String TAG = "tr.lib.e.PlaneResult";
+
+    public static final int BATTERY = 401;
+    public static final int RUDER = 402;
+    public static final int MOTOR = 403;
+
+
+    private int device,value;
+
+
+    public PlaneResult(int device,int value) {
+        this.device = device;
+        this.value = value;
+    }
+
+    public int getDevice(){
+        return device;
+    }
+
+    public int getValue(){
+        return value;
+    }
+}
