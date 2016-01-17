04efc8fc2a2d9de50b1b87c45b33378610f8cc2e
ygallwas <ygallwas@stud.hs-bremen.de>
2016-01-17 21:32:09 +0100

    fixed unit test bugs

--- HangarApp/src/test/java/com/tobyrich/dev/hangarapp/activities/AchievementsActivityTest.java
+++ HangarApp/src/test/java/com/tobyrich/dev/hangarapp/activities/AchievementsActivityTest.java
@@ -1,13 +1,10 @@
 package com.tobyrich.dev.hangarapp.activities;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.util.LruCache;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -15,10 +12,6 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.google.inject.AbstractModule;
-import com.tobyrich.dev.hangarapp.R;
-import com.tobyrich.dev.hangarapp.adapters.AchievementsAdapter;
-import com.tobyrich.dev.hangarapp.beans.api.feeders.AchievementsFeeder;
-import com.tobyrich.dev.hangarapp.beans.api.model.Achievement;
 import com.tobyrich.dev.hangarapp.lib.BuildConfig;
 
 import org.junit.Assert;
@@ -30,25 +23,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.Shadows;
 import org.robolectric.annotation.Config;
 import org.robolectric.shadows.ShadowConnectivityManager;
-import org.robolectric.shadows.ShadowListView;
 import org.robolectric.shadows.ShadowNetworkInfo;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import roboguice.RoboGuice;
 import roboguice.inject.RoboInjector;
-
-import static org.mockito.Matchers.anyInt;
-import static org.mockito.Matchers.anyObject;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.times;
 
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class)
@@ -59,20 +42,16 @@ public class AchievementsActivityTest extends Mockito {
     @Mock private TextView tvDescription;
     @Mock private ImageView ivSmartplane;
     @Mock private ProgressBar achievementsLoading;
+    @Mock private ListView lvAchievements;
 
     @Mock private View v;
     @Mock private LruCache<String, Bitmap> mMemoryCache;
 
-    private AchievementsAdapter adapter;
-    private List<Achievement> achievementList = new ArrayList<Achievement>();
     private ConnectivityManager connectivityManager;
     private ShadowConnectivityManager shadowConnectivityManager;
     private ShadowNetworkInfo shadowOfActiveNetworkInfo;
-    private AchievementsFeeder achievementsFeeder;
 
     private AchievementsActivity activity;
-
-    private ListView lvAchievements;
 
     @Before
     public void setUp() throws Exception {
@@ -92,11 +71,6 @@ public class AchievementsActivityTest extends Mockito {
         connectivityManager = (ConnectivityManager) RuntimeEnvironment.application.getSystemService(Context.CONNECTIVITY_SERVICE);
         shadowConnectivityManager = Shadows.shadowOf(connectivityManager);
         shadowOfActiveNetworkInfo = Shadows.shadowOf(connectivityManager.getActiveNetworkInfo());
-        achievementsFeeder = new AchievementsFeeder(achievementsActivity,achievementsActivity.getApplicationContext(),"1111");
-        achievementList = achievementsFeeder.getAchievementsList();
-        adapter = new AchievementsAdapter(achievementsActivity.getApplicationContext(),achievementList);
-        lvAchievements.setAdapter(adapter);
-        //lvAchievements.addView(adapter.getView(0, LayoutInflater.from(achievementsActivity.getApplicationContext()).inflate(R.layout.achievement, null), null));
     }
 
     @Ignore
